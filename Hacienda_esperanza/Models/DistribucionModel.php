4535032e512c22f683326341dffafc2347c6a586
LEONELXD <69255431+LEONELXD@users.noreply.github.com>
2023-01-07 17:33:50 -0500

    haciendaxd

---
+++ Hacienda_esperanza/Models/DistribucionModel.php
@@ -0,0 +1,59 @@
+
+<?php 
+
+class DistribucionModel extends Mysql
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function seleccionPedidos(){
+		$fecha= date('Y-m-d');
+		$fechastring=strval($fecha);
+		$sql = "SELECT p.idpedido,
+				p.personaid,
+				p.direccion_envio,
+				DATE_FORMAT(p.fecha, '%d/%m/%Y') as fecha,
+				p.monto,
+				p.status,
+				p.personaid,
+				p.latitud,
+				p.longitud,
+				per.nombres,
+				per.apellidos,
+				per.telefono
+		FROM pedido p 
+		INNER JOIN persona per
+		ON p.personaid = per.idpersona 
+		where
+		`fecha` like '$fechastring%' and 
+            p.status not in ('Cancelado','Pendiente') 
+		 ";
+		$request = $this->select_all($sql);
+		return $request;
+
+
+	}
+
+	public function obtenerproductos($idpedido){
+		$sql = "SELECT dp.*,
+				pro.nombre
+
+		FROM detalle_pedido dp 
+		INNER JOIN producto pro
+		ON dp.productoid = pro.idproducto
+		where dp.pedidoid=$idpedido  ";
+		$request = $this->select_all($sql);
+		return $request;
+
+	}
+
+
+
+
+
+}
+
+
+?>
